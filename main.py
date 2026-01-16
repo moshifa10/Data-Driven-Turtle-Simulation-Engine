82c00a77a6b0348ea61ca1539019d69bba9c3025
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-01-16 10:14:24 +0200

    created

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+🧠 Challenge: Data-Driven Turtle Simulation Engine
+🏁 Project Goal
+
+Build a command-line program that:
+
+Reads data from a file
+
+Analyzes it using pandas
+
+Makes decisions using algorithms
+
+Visualizes the results using Turtle graphics
+
+This mimics real data workflows:
+
+CLI → Data → Analysis → Decision → Visualization'''
